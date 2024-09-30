c5161e1643b843c69c52241e30ead16852b0daff
Ahmed Samir Mohamed <ahmedsamer65491@gmail.com>
2024-09-30 10:28:36 +0300

    finish design

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,49 @@
-import { Text, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  ListRenderItem,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { data } from "../assets/data";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function Index() {
+  const renderItem: ListRenderItem<any> = ({ item }) => (
+    <View style={{ flexDirection: "row", padding: 8, flex: 1 }}>
+      <View style={{ flexDirection: "row" }}>
+        <Image source={{ uri: item.image }} style={{ width: 75, height: 75 }} />
+        <View style={{ marginLeft: 10 }}>
+          <Text style={{ width: 200 }}>{item.title}</Text>
+          <Text style={{ color: "gray" }}>Price: ${item.price}</Text>
+        </View>
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          flex: 1,
+          justifyContent: "flex-end",
+        }}
+      >
+        <TouchableOpacity style={{ padding: 10 }}>
+          <Ionicons name="remove" size={24} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity style={{ padding: 10 }}>
+          <Ionicons name="add" size={24} color="black" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
   return (
-    <View
+    <SafeAreaView
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        marginTop: 16,
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+      <FlatList data={data} renderItem={renderItem} />
+    </SafeAreaView>
   );
 }
